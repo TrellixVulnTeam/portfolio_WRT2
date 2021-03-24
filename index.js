549049f8b5daa9027ea2a6d1c236810659652733
juplmn777 <julien.plumyoen@gmail.com>
2021-03-24 14:38:17 +0100

    navbar burger js (closing navbar by clicking navbar-burger links)

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ const allNavLinks = document.querySelectorAll('.navbar-item');
 /* burger menu */
 navBtn.addEventListener('click', () => {
   menuContent.classList.toggle('is-active');
+  navBtn.classList.toggle('is-active');
 });
 
-
+/* All nav links closing burger-navbar*/
+allNavLinks.forEach((navItem) => {
+  navItem.addEventListener('click', () => {
+    menuContent.classList.toggle('is-active');
+  });
+});
